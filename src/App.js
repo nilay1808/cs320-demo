74c7fd05450b78f3735ac4c6a440d9e3dc058774
Nilay Sadavarte <nilay18sadavarte@gmail.com>
2020-10-19 14:58:42 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
-import { Container, Navbar, Image } from "react-bootstrap";
+import React from 'react';
+import { Container, Navbar, Image } from 'react-bootstrap';
 
-import "./styles/App.css";
-import dogImg from "./assets/dog.jpg";
+import './styles/App.css';
+import dogImg from './assets/dog.jpg';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Navbar bg="dark" variant="dark">
-					<Container>
-						<Navbar.Brand href="#home">Demo App</Navbar.Brand>
-					</Container>
-				</Navbar>
-				<Container>
-					<div
-						style={{
-							marginTop: "3em",
-						}}
-					>
-						<Image style={{ maxWidth: "100%" }} src={dogImg} />
-					</div>
-				</Container>
-			</div>
-		);
-	}
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Demo App</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <div
+          style={{
+            marginTop: '3em',
+          }}
+        >
+          <Image style={{ maxWidth: '100%' }} src={dogImg} />
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
